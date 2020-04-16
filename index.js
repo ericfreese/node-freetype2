7eae6441a3bdda7a9528d60040c91bb2a0cf1f3e
Julian Waller <git@julusian.co.uk>
2020-04-16 20:18:31 +0100

    chore: update some docs and tidying

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const ft = require('./build/Release/freetype2');
-
-console.log(ft)
-
-const face = ft.NewFace('./spec/fonts/OpenBaskerville-0.0.53/OpenBaskerville-0.0.53.ttf')
-console.log(face, JSON.stringify(face))
-console.log(face.numFaces, face.faceIndex, face.properties())
-console.log(face.getCharIndex(56))
+module.exports = require('bindings')('freetype2.node')
