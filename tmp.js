370498db053d4156fc2262694dd3bda783067d4b
Julian Waller <git@julusian.co.uk>
2020-04-19 15:37:24 +0100

    chore: update readme

--- tmp.js
+++
@@ -1,8 +0,0 @@
-const ft = require('.');
-
-console.log(ft)
-
-const face = ft.NewFace('./spec/fonts/OpenBaskerville-0.0.53/OpenBaskerville-0.0.53.ttf')
-console.log(face, JSON.stringify(face))
-console.log(face.numFaces, face.faceIndex, face.properties())
-console.log(face.getCharIndex(56))
