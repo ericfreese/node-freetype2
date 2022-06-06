ca038950b883d35804435cc9b59b79f7f1a3064c
Julian Waller <git@julusian.co.uk>
2022-06-06 21:01:09 +0100

    fix: thread safety

--- src/util.h
+++ src/util.h
@@ -1,4 +1,7 @@
-#define NAPI_VERSION 3
+#ifndef NODE_FREETYPE2_UTIL_H
+#define NODE_FREETYPE2_UTIL_H
+
+#define NAPI_VERSION 6
 #include <napi.h>
 
 #include <ft2build.h>
@@ -11,3 +14,10 @@ bool validateProp(const Napi::Env& env, bool isCorrect, const char* propName);
 bool validatePropsLength(const Napi::Env& env, const Napi::CallbackInfo &info, unsigned int minLength);
 
 bool checkProperty(const Napi::Object& obj, const char* name);
+
+struct InstanceData {
+  FT_Library library;
+  Napi::FunctionReference fontFace;
+};
+
+#endif
