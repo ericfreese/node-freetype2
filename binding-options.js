1b4c7ac5597a3f19304828ab7aee297f4f0d6214
Julian Waller <git@julusian.co.uk>
2022-09-28 10:31:21 +0100

    feat: replace node-gyp-build with pkg-prebuilds (#2)
    Co-authored-by: Hannes Rüger <hannesrueger@gmx.de>

---
+++ binding-options.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'freetype2',
+  napi_versions: [7],
+}
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Will load a compiled build if present or a prebuild.
 // If no build if found it will throw an exception
-module.exports = require('node-gyp-build')(__dirname)
+module.exports = require("pkg-prebuilds/bindings")(
+  __dirname,
+  require("./binding-options")
+);
